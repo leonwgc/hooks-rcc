45d5e93a68b5a9cd230736f15e62ad02a6f63476
leonwgc <giantfish@126.com>
2020-12-07 10:54:55 +0800

    toggle sig

--- src/h5/sig/Sig.jsx
+++ src/h5/sig/Sig.jsx
@@ -1,12 +1,21 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import PortraitSignature from '../signature/PortraitSignature';
+import { Icon, TabBar, Cell, Button } from 'zarm';
 import LandscapeSignature from '../signature/LandscapeSignature';
 
 export default function Sig() {
+  const [isLandscape, setisLandscape] = useState(false);
+  const [sigData, setSigData] = useState(null);
   return (
     <div>
-      {/* <PortraitSignature /> */}
-      <LandscapeSignature />
+      <Button
+        onClick={() => {
+          setisLandscape((l) => !l);
+        }}
+      >
+        {isLandscape ? 'land' : 'port'}
+      </Button>
+      {isLandscape ? <LandscapeSignature /> : <PortraitSignature />}
     </div>
   );
 }
