058af6954627044d0c85836a88f7d17bf733d249
leonwgc <giantfish@126.com>
2021-01-22 12:36:43 +0800

    keyboard listen

--- src/vw/demo/Page1.jsx
+++ src/vw/demo/Page1.jsx
@@ -2,9 +2,21 @@ import React, { useEffect, useRef, useState } from 'react';
 import './Page1.less';
 
 export default function Page1() {
+  useEffect(() => {
+    window.addEventListener('keydown', (e) => {
+      console.log({
+        altKey: e.altKey,
+        ctrlKey: e.ctrlKey,
+        shiftKey: e.shiftKey,
+        metaKey: e.metaKey,
+        which: e.which,
+      });
+    });
+  }, []);
+
   return (
     <div className="page1">
-      <div className="rect">hello,world</div>
+      <div className="rect"></div>
     </div>
   );
 }
