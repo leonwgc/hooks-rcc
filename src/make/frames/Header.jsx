ac6373e7e7590a09e6a895e6f412492198728bb5
leonwgc <giantfish@126.com>
2021-02-03 15:51:39 +0800

    update

--- src/make/frames/Header.jsx
+++ src/make/frames/Header.jsx
@@ -1,17 +1,17 @@
-import React, {useState} from 'react';
-import updateTheme, {defaultBrandColor} from '~/make/Theme';
+import React, { useState } from 'react';
+import updateTheme, { defaultBrandColor } from '~/make/Theme';
 import './Header.less';
 
 export default function Header() {
   const [color, setColor] = useState(defaultBrandColor);
-  const onChange = e => {
-    updateTheme({brandColor: e.target.value});
+  const onChange = (e) => {
+    updateTheme({ brandColor: e.target.value });
     setColor(e.target.value);
   };
   return (
     <div className="ns-header">
-      web design
-      <input type="color" onChange={onChange} className="color" value={color} />
+      <div className="title">装修管理台</div>
+      {/* <input type="color" onChange={onChange} className="color" value={color} /> */}
     </div>
   );
 }
